ded94d199f2128aca5382f2694f0dd2b85ce6503
suribabu <suri.babu988@gmail.com>
2018-08-24 17:46:37 +0530

    added ne files..

--- js/main.js
+++ js/main.js
@@ -2,41 +2,41 @@ $(document).ready(function() {
 //menu mouseovr dropdown logic
 	$('.dropdown').on('mouseover', function(){
 		$(this).find('.dropdown-menu').show();
-		});
-		$('.dropdown').on('mouseout', function(){
-			$(this).find('.dropdown-menu').hide();
-		});
+	});
+	$('.dropdown').on('mouseout', function(){
+		$(this).find('.dropdown-menu').hide();
+	});
 
 //home page in image on mouseover animation logic
     $(".img-container").on('mouseenter', function(){
-    $(this).find('.overlay').css('display','block');
-    var play=  $(this).find('.play-icon');
-    var film=  $(this).find('.film');
-    play.animate({
-    top: '-20%'
-    }, "fast");
-    film.animate({
-    top: '100%'
-    }, "slow");
+		$(this).find('.overlay').css('display','block');
+		var play=  $(this).find('.play-icon');
+		var film=  $(this).find('.film');
+		play.animate({
+		top: '-20%'
+		}, "fast");
+		film.animate({
+		top: '100%'
+		}, "slow");
     });
 
     $(".img-container").on('mouseleave', function(){
-    $(this).find('.overlay').css('display','none');
-    var play=  $(this).find('.play-icon');
-    var film=  $(this).find('.film');
-    play.animate({
-    top: '25%'
-    }, "fast");
-    film.animate({
-    top: '60%'
-    }, "slow");
+		$(this).find('.overlay').css('display','none');
+		var play=  $(this).find('.play-icon');
+		var film=  $(this).find('.film');
+		play.animate({
+		top: '25%'
+		}, "fast");
+		film.animate({
+		top: '60%'
+		}, "slow");
 	});
 
 // Gets the video src from the data-src on each button
     var $imageSrc3;
 	$('.modalvideo a').on('click',function() {
-			var $this = $(this);
-			$imageSrc3 = $(this).data('commvideo');
+		var $this = $(this);
+		$imageSrc3 = $(this).data('commvideo');
 //next and prev button's hide or show logic	
 		if($this.next('.modalvideo a').attr('data-commvideo') === undefined)
 			{
@@ -48,24 +48,24 @@ $(document).ready(function() {
 			$('#prev-btn').show();
 		}
 		if($this.data('num') == 'first'){
-				$('#prev-btn').hide();
-				$('#next-btn').show();
-			}
-			if($this.data('num') == 'last'){
-				$('#next-btn').hide();
-				$('#prev-btn').show();
-			}
+			$('#prev-btn').hide();
+			$('#next-btn').show();
+		}
+		if($this.data('num') == 'last'){
+			$('#next-btn').hide();
+			$('#prev-btn').show();
+		}
 
 // next click logic			
 		$('#next-btn').on('click', function() {
 			$next = $this.next('.modalvideo a').attr('data-commvideo');
 			$this = $this.next('.modalvideo a');
-					$('.modal-body #commimage').attr('src', $next+'?autoplay=1&showinfo=0&controls=0&autohide=1');
-		if($this.data('num') == 'last'){
+			$('.modal-body #commimage').attr('src', $next+'?autoplay=1&showinfo=0&controls=0&autohide=1');
+			if($this.data('num') == 'last'){
 				$(this).hide();
 				$('#prev-btn').show();
 			}
-		if($this.data('num') != 'last'){
+			if($this.data('num') != 'last'){
 				$(this).show();
 				$('#prev-btn').show();
 			}
@@ -92,7 +92,6 @@ $(document).ready(function() {
 	});
 // reset the modal image
     $('#commModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
         $("#commimage").attr('src','');
     })
 });// document ready
